387e79a7545466f7a0010869d25fa19436e1c1dd
ankurnepalia <ankur.nepalia@gmail.com>
2016-06-16 21:27:49 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,17 +1,17 @@
-#read full household_power_consumption.txt file in a table
+# Read full household_power_consumption.txt file in a table
 full_data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", dec = ".", na.strings = "?", stringsAsFactors = FALSE)
 
-#Subset full_data to just include data from dates 2007-02-01 and 2007-02-02
+# Subset full_data to just include data from dates 2007-02-01 and 2007-02-02
 relevant_data <- full_data[full_data$Date %in% c("1/2/2007", "2/2/2007"), ]
 
-#Creating numeric field
+# Creating numeric field
 globalActivePower <- as.numeric(relevant_data$Global_active_power)
 
-#Drawing Plot 1
+# Drawing Plot 1
 hist(globalActivePower, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency")
 
-#Saving Plot1 to png file
+# Saving Plot1 to png file
 dev.copy(png, file = "plot1.png", height = 480, width = 480)
 
-#close graphical device
+# Close graphical device
 dev.off()
